1f8fbd75924a0654c4f6fba642000c082abdd551
Isabelle BENGRINE <isabelle.bengrine@talan.com>
2021-02-03 12:56:54 +0100

    Added member editpassword method and twig

--- BenGreen/src/Controller/MemberController.php
+++ BenGreen/src/Controller/MemberController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /** @Route("/member") */
 class MemberController extends AbstractController
@@ -41,6 +42,37 @@ class MemberController extends AbstractController
 
         return $this->render('member/editaccount.html.twig', [
             'form' => $form->createView(),
+            'mainNavMember'=>true,
+            'title'=>'Espace Membre',
+        ]);
+    }
+
+    /**
+     * @Route("/userinfo/pass/edit", name="member_userpass_edit")
+     */
+    public function editUserPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        if($request->isMethod('POST')){
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+
+            //to check if both passwords are identical :
+            if($request->get('pass') == $request->get('pass2')){
+
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->get('pass')));
+                $entityManager->flush();
+
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès!');
+
+                return $this->redirectToRoute('member_userinfo');
+
+            } else {
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques, vérifiez votre saisie.');
+            }
+
+        }
+
+        return $this->render('member/editpassword.html.twig', [
             'mainNavMember'=>true,
             'title'=>'Espace Membre',
         ]);
