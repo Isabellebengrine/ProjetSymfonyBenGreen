e44f6c15913663477e3ca974333a6f3d6c558d1c
Isabelle BENGRINE <isabelle.bengrine@talan.com>
2021-02-03 10:27:46 +0100

    Added links and twigs used in member pages

--- BenGreen/src/Controller/MemberController.php
+++ BenGreen/src/Controller/MemberController.php
@@ -6,14 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /** @Route("/member") */
-class MemberController extends AbstractController {
-
-/**
-* @Route("/")
-*/
-public function index()
+class MemberController extends AbstractController
 {
-    return $this->render('member/index.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
-}
+
+    /**
+    * @Route("/", name="member_home")
+    */
+    public function index()
+    {
+        return $this->render('member/index.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
+    }
+
+    /**
+     * @Route("/userinfo", name="member_userinfo")
+     */
+    public function showUserInfo()
+    {
+        return $this->render('member/user.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
+    }
+
+    /**
+     * @Route("/customer", name="member_customer")
+     */
+    public function showCustomerInfo()
+    {
+        return $this->render('member/customer.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
+    }
+
+    /**
+     * @Route("/orders", name="member_orders")
+     */
+    public function showOrders()
+    {
+        return $this->render('member/orders.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
+    }
 
 }
