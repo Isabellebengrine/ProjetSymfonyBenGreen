0359f9b80dcc2f177556dbb6e8faa3dbd3869d31
Isabelle Bengrine <ibengrine@yahoo.com>
2021-02-22 08:39:00 +0100

    Added CartManager to retrieve current cart and save it in database and in session

---
+++ BenGreen/src/Manager/CartManager.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\Totalorder;
+use App\Factory\OrderFactory;
+use App\Storage\CartSessionStorage;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Class CartManager
+ * @package App\Manager
+ */
+class CartManager
+{
+    /**
+     * @var CartSessionStorage
+     */
+    private $cartSessionStorage;
+
+    /**
+     * @var OrderFactory
+     */
+    private $cartFactory;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * CartManager constructor.
+     *
+     * @param CartSessionStorage $cartStorage
+     * @param OrderFactory $orderFactory
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(
+        CartSessionStorage $cartStorage,
+        OrderFactory $orderFactory,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->cartSessionStorage = $cartStorage;
+        $this->cartFactory = $orderFactory;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Gets the current cart (use the CartSessionStorage to get the cart already associated with the user;
+     * if the cart does not exist yet, then create a new one with the OrderFactory create() method.
+     * @return Totalorder
+     */
+    public function getCurrentCart(): Totalorder
+    {
+        $cart = $this->cartSessionStorage->getCart();
+
+        if (!$cart) {
+            $cart = $this->cartFactory->create();
+        }
+
+        return $cart;
+    }
+
+    /**
+     * Persists the cart in database and session.
+     *
+     * @param Totalorder $cart
+     */
+    public function save(Totalorder $cart): void
+    {
+        // Persist in database
+        $this->entityManager->persist($cart);
+        $this->entityManager->flush();
+        // Persist in session
+        $this->cartSessionStorage->setCart($cart);
+    }
+
+
+}
+
+
+
+
+
